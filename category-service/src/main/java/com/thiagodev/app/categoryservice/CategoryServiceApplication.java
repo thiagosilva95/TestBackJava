154f8f52345aeb56dc4fef500af9e5b81b1ac52d
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-10-15 22:08:17 -0300

    initial commit

---
+++ category-service/src/main/java/com/thiagodev/app/categoryservice/CategoryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.thiagodev.app.categoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryServiceApplication.class, args);
+	}
+}
