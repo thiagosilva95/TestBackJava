8eb657265a6056292181a0ecee1b8d05c694565e
Thiago Henrique <thiagonascimento95@hotmail.com>
2018-10-15 22:51:53 -0300

    centralizing config

--- netflix-eureka-naming-service/src/main/java/com/thiagodev/app/netflixeurekanamingservice/NetflixEurekaNamingServiceApplication.java
+++ netflix-eureka-naming-service/src/main/java/com/thiagodev/app/netflixeurekanamingservice/NetflixEurekaNamingServiceApplication.java
@@ -2,7 +2,9 @@ package com.thiagodev.app.netflixeurekanamingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class NetflixEurekaNamingServiceApplication {
 
